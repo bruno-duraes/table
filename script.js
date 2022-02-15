7446b3d8eaf65e6a5c465ec3268c562796f98d8f
Bruno Peterson Durães <brunodur4es@gmail.com>
2022-02-15 12:12:47 -0300

    Added tfoot

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function addData() {
     unitValue.innerHTML = `<input type="text" class="td-input unit-value" value="R$${unitValueV}" readonly>`
 
     let totalValue = document.createElement('td')
-    totalValue.innerHTML = `<input type="text" class="td-input total-value" value="R$${calculateTotalvalue(qntdV, unitValueV)}" readonly>`
+    totalValue.innerHTML = `<input type="text" class="td-input total-value" value="R$${calculateTotalValue(qntdV, unitValueV)}" readonly>`
 
     let btn = document.createElement('td')
     btn.innerHTML = `<div class="table-btns">
-    <i class="bi bi-pencil-square" id="edit-btn"></i>
+    <i class="bi bi-pencil-square" id="edit-btn" onclick="editLine(${idV})"></i>
     <i class="bi bi-x-square-fill" id="delete-btn" onclick="deleteLine(${idV})"></i>
     </div>`
     btn.setAttribute('style', 'padding: 0')
@@ -43,11 +43,19 @@ function addData() {
     document.querySelector('#name').value = ''
     document.querySelector('#unit-value').value = ''
 
-    function calculateTotalvalue(num1, num2) {
-        num1 = parseFloat(num1.replace(",", "."))
-        num2 = parseFloat(num2.replace(",", "."))
-        let result = num1 * num2
-        return result.toString().replace('.', ',')
+    function calculateTotalValue(num1, num2) {
+
+        if (num2.toString().indexOf(',') > 0) {
+            num1 = parseInt(num1.replace(",", "."))
+            num2 = parseFloat(num2.replace(",", "."))
+            let result = num1 * num2
+            resultDecimal = `${Math.round(parseFloat(result.toString().split('.')[1].split('').slice(0, 2).join('.')))}${0}`
+            resultInt = result.toString().split('.')[0]
+            result = `${resultInt},${resultDecimal}`
+            return result.toString()
+        } else {
+            return num1 * num2
+        }
     }
 }
 
@@ -58,5 +66,22 @@ function deleteLine(i) {
     )
 }
 
+function editLine(obj) {
 
+    let allElements = document.querySelector(`#row${obj}`).children
 
+    if (allElements[0].children[0].hasAttribute('readonly')) {
+        for (let i = 0; i < 4; i++) {
+
+            let element = allElements[i].children
+            element[0].removeAttribute('readonly')
+        }
+    } else {
+        for (let i = 0; i < 4; i++) {
+
+            let element = allElements[i].children
+            element[0].setAttribute('readonly', 'readonly')
+        }
+    }
+
+}
