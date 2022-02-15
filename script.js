1860bca5d58e264e93d23584670073086930ab1d
Bruno Peterson Durães <brunodur4es@gmail.com>
2022-02-15 18:00:59 -0300

    Id Validated

--- script.js
+++ script.js
@@ -1,75 +1,97 @@
-this.workflowCockpit = workflowCockpit({
-    init: _loadData,
-    onSubmit: _saveData,
-    onError: _rollback
-});
+// this.workflowCockpit = workflowCockpit({
+//     init: _loadData,
+//     onSubmit: _saveData,
+//     onError: _rollback
+// });
+
+let arrayId = []
+
+function getId() {
+
+    let arrayStr = arrayId.toString().split(',')
+
+    let n = 1
+    while (arrayStr.indexOf(`${n}`) >= 0) {
+        arrayStr.indexOf(`${n}`) === -1 ? arrayId.push(n) : null
+        n++
+    }
+    arrayId.push(n)
+    return n
+}
 
 function addData() {
 
-    let idV = document.querySelector('.table').getElementsByTagName('tr').length
-    let qntdV = document.querySelector('#qntd').value
-    let nameV = document.querySelector('#name').value
-    let unitValueV = document.querySelector('#unit-value').value
+    let rowsLenght = document.querySelector('#tbody').children.length
 
-    console.log(idV, qntdV, nameV, document.querySelector('#tbody').getElementsByTagName('tr'))
+    if (rowsLenght < 10) {
+        let qntdV = document.querySelector('#qntd').value
+        let nameV = document.querySelector('#name').value
+        let unitValueV = document.querySelector('#unit-value').value
+        let idV = getId()
+        console.log(idV, arrayId)
 
-    let tbody = document.querySelector('#tbody')
+        let tbody = document.querySelector('#tbody')
 
-    let row = document.createElement('tr')
-    row.setAttribute('id', `row${idV}`)
+        let row = document.createElement('tr')
+        row.setAttribute('id', `row${idV}`)
 
-    let qntd = document.createElement('td')
-    qntd.innerHTML = `<input type="text" class="td-input qntd" value="${qntdV}" readonly>`
+        let qntd = document.createElement('td')
+        qntd.innerHTML = `<input type="text" class="td-input qntd" value="${qntdV}" readonly>`
 
-    let name = document.createElement('td')
-    name.innerHTML = `<input type="text" class="td-input name" value="${nameV}" readonly>`
+        let name = document.createElement('td')
+        name.innerHTML = `<input type="text" class="td-input name" value="${nameV}" readonly>`
 
-    let unitValue = document.createElement('td')
-    unitValue.innerHTML = `<input type="text" class="td-input unit-value" value="R$${unitValueV}" readonly>`
+        let unitValue = document.createElement('td')
+        unitValue.innerHTML = `<input type="text" class="td-input unit-value" value="R$${unitValueV}" readonly>`
 
-    let totalValue = document.createElement('td')
-    totalValue.innerHTML = `<input type="text" class="td-input total-value" value="R$${calculateTotalValue(qntdV, unitValueV)}" readonly>`
+        let totalValue = document.createElement('td')
+        totalValue.innerHTML = `<input type="text" class="td-input total-value" value="R$${calculateTotalValue(qntdV, unitValueV)}" readonly>`
 
-    let btn = document.createElement('td')
-    btn.innerHTML = `<div class="table-btns">
-    <i class="bi bi-pencil-square" id="edit-btn" onclick="editLine(${idV})"></i>
-    <i class="bi bi-x-square-fill" id="delete-btn" onclick="deleteLine(${idV})"></i>
-    </div>`
-    btn.setAttribute('style', 'padding: 0')
+        let btn = document.createElement('td')
+        btn.innerHTML = `<div class="table-btns">
+        <i class="bi bi-pencil-square" id="edit-btn" onclick="editLine(${idV})"></i>
+        <i class="bi bi-x-square-fill" id="delete-btn" onclick="deleteLine(${idV})"></i>
+        </div>`
+        btn.setAttribute('style', 'padding: 0')
 
-    tbody.appendChild(row)
-    row.appendChild(qntd)
-    row.appendChild(name)
-    row.appendChild(unitValue)
-    row.appendChild(totalValue)
-    row.appendChild(btn)
+        tbody.appendChild(row)
+        row.appendChild(qntd)
+        row.appendChild(name)
+        row.appendChild(unitValue)
+        row.appendChild(totalValue)
+        row.appendChild(btn)
 
-    //  Limpando os campos
-    document.querySelector('#qntd').value = ''
-    document.querySelector('#name').value = ''
-    document.querySelector('#unit-value').value = ''
+        // idNumber++
 
-    function calculateTotalValue(num1, num2) {
+        //  Limpando os campos
+        document.querySelector('#qntd').value = ''
+        document.querySelector('#name').value = ''
+        document.querySelector('#unit-value').value = ''
 
-        if (num2.toString().indexOf(',') > 0) {
-            num1 = parseInt(num1.replace(",", "."))
-            num2 = parseFloat(num2.replace(",", "."))
-            let result = num1 * num2
-            resultDecimal = `${Math.round(parseFloat(result.toString().split('.')[1].split('').slice(0, 2).join('.')))}${0}`
-            resultInt = result.toString().split('.')[0]
-            result = `${resultInt},${resultDecimal}`
-            return result.toString()
-        } else {
-            return num1 * num2
+        function calculateTotalValue(num1, num2) {
+
+            if (num2.toString().indexOf(',') > 0) {
+                num1 = parseInt(num1.replace(",", "."))
+                num2 = parseFloat(num2.replace(",", "."))
+                let result = num1 * num2
+                resultDecimal = `${Math.round(parseFloat(result.toString().split('.')[1].split('').slice(0, 2).join('.')))}${0}`
+                resultInt = result.toString().split('.')[0]
+                result = `${resultInt},${resultDecimal}`
+                return result.toString()
+            } else {
+                return num1 * num2
+            }
         }
+
+    } else {
+        alert('Numero de linhas excedido ☺')
     }
 }
 
-
 function deleteLine(i) {
-    console.log(
-        document.querySelector(`#row${i}`).remove()
-    )
+
+    document.querySelector(`#row${i}`).remove()
+    arrayId.splice(arrayId.indexOf(i), 1)
 }
 
 function editLine(obj) {
