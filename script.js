42e2043525315af520610f3b2ad15f5ff8760316
Bruno Peterson Durães <brunodur4es@gmail.com>
2022-02-17 14:38:40 -0300

    add modals

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// this.workflowCockpit = workflowCockpit({
-//     init: _loadData,
-//     onSubmit: _saveData,
-//     onError: _rollback
-// });
+this.workflowCockpit = workflowCockpit({
+    //     init: _loadData,
+    onSubmit: _saveData,
+    //     onError: _rollback
+});
 
 let arrayId = []
+let totalPrice = 0
 
 function getId() {
 
@@ -19,82 +20,103 @@ function getId() {
     return n
 }
 
-function addData() {
+function insertsValidation() {
+    let quantity = document.querySelector('#qntd').value
+    let description = document.querySelector('#name').value
+    let unitValue = document.querySelector('#unit-value').value
 
-    let rowsLenght = document.querySelector('#tbody').children.length
-
-    if (rowsLenght < 10) {
-        let qntdV = document.querySelector('#qntd').value
-        let nameV = document.querySelector('#name').value
-        let unitValueV = document.querySelector('#unit-value').value
-        let idV = getId()
-
-        let tbody = document.querySelector('#tbody')
-
-        let row = document.createElement('tr')
-        row.setAttribute('id', `row${idV}`)
-
-        let qntd = document.createElement('td')
-        qntd.innerHTML = `<div class="td-div" id="td-edit">
-        <input type="number" class="td-input qntd form-control" value="${qntdV}" min="0" max="100" readonly required>
-        <div>`
-
-        let name = document.createElement('td')
-        name.innerHTML = `<div class="td-div" id="td-edit">
-        <i class="bi bi-caret-right-fill"></i>
-        <input type="text" class="td-input name form-control" value="${nameV}" readonly required>
-        </div`
-
-        let unitValue = document.createElement('td')
-        unitValue.innerHTML = `<div class="td-div" id="td-edit">
-        <span style="padding-top: 3px">R$</span>
-        <input type="number" class="td-input unit-value form-control" value="${unitValueV}" readonly required>
-        </div>`
-
-        let totalValue = document.createElement('td')
-        totalValue.innerHTML = `<div class="td-div" id="td-total-value">
-        <span style="padding-top: 3px">R$</span><input type="text" class="td-input total-value form-control" value="${calculateTotalValue(qntdV, unitValueV)}" readonly required>
-        </div>`
-        let btn = document.createElement('td')
-        btn.innerHTML = `<div class="table-btns">
-        <i class="bi bi-pencil-square" id="edit-btn" onclick="editLine(${idV})"></i>
-        <i class="bi bi-x-square-fill" id="delete-btn" onclick="deleteLine(${idV})"></i>
-        </div>`
-        btn.setAttribute('style', 'padding: 0')
-
-        tbody.appendChild(row)
-        row.appendChild(qntd)
-        row.appendChild(name)
-        row.appendChild(unitValue)
-        row.appendChild(totalValue)
-        row.appendChild(btn)
-
-        addTotalValue()
-
-        //  Limpando os campos
-        document.querySelector('#qntd').value = ''
-        document.querySelector('#name').value = ''
-        document.querySelector('#unit-value').value = ''
-
-    } else {
-        alert('Numero de linhas excedido ☺')
+    if (!quantity || !description || !unitValue) {
+        return ("invalid")
+    } {
+        return ("valid")
     }
 }
 
-let totalPrice = 0
+function shootModal(str) {
+    let modal_el = document.querySelector(`#${str}`)
+    let modal = new bootstrap.Modal(modal_el)
+    modal.show()
+}
+
+
+function addData() {
+    if (insertsValidation() == 'invalid') {
+        shootModal('modal-insertValidation')
+    }
+    else {
+
+        let rowsLenght = document.querySelector('#tbody').children.length
+
+        if (rowsLenght < 10) {
+            let qntdV = document.querySelector('#qntd').value
+            let nameV = document.querySelector('#name').value
+            let unitValueV = document.querySelector('#unit-value').value
+            let idV = getId()
+
+            let tbody = document.querySelector('#tbody')
+
+            let row = document.createElement('tr')
+            row.setAttribute('id', `row${idV}`)
+
+            let qntd = document.createElement('td')
+            qntd.innerHTML = `<div class="td-div" id="td-edit">
+        <input id="input-qnt-${idV}" type="number" class="td-input qntd form-control" value="${qntdV}" min="0" max="100" readonly required>
+        <div>`
+
+            let name = document.createElement('td')
+            name.innerHTML = `<div class="td-div" id="td-edit">
+        <i class="bi bi-caret-right-fill"></i>
+        <input id="input-name-${idV}" type="text" class="td-input name form-control" value="${nameV}" readonly required>
+        </div`
+
+            let unitValue = document.createElement('td')
+            unitValue.innerHTML = `<div class="td-div" id="td-edit">
+        <span style="padding-top: 3px">R$</span>
+        <input id="input-unitValue-${idV}" type="number" class="td-input unit-value form-control" value="${unitValueV}" readonly required>
+        </div>`
+
+            let totalValue = document.createElement('td')
+            totalValue.innerHTML = `<div class="td-div" id="td-total-value">
+        <span style="padding-top: 3px">R$</span>
+        <input id="input-totalValue-${idV}" type="text" class="td-input total-value form-control" value="${calculateTotalValue(qntdV, unitValueV)}" readonly required>
+        </div>`
+            let btn = document.createElement('td')
+            btn.innerHTML = `<div class="table-btns">
+        <i class="bi bi-pencil-square" id="edit-btn" onclick="editLine(${idV})"></i>
+        <i class="bi bi-x-square-fill" id="delete-btn" onclick="deleteLine(${idV})"></i>
+        </div>`
+            btn.setAttribute('style', 'padding: 0')
+
+            tbody.appendChild(row)
+            row.appendChild(qntd)
+            row.appendChild(name)
+            row.appendChild(unitValue)
+            row.appendChild(totalValue)
+            row.appendChild(btn)
+
+            addTotalValue()
+
+            //  Limpando os campos
+            document.querySelector('#qntd').value = ''
+            document.querySelector('#name').value = ''
+            document.querySelector('#unit-value').value = ''
+
+        } else {
+            shootModal('modal-maxLines')
+        }
+    }
+}
 
 function addTotalValue() {
 
     let totalValue = document.querySelectorAll('.td-input.total-value')
     let displayValue = document.querySelector('#display-value').innerHTML
-    console.log(displayValue)
     for (let i = 0; i < totalValue.length; i++) {
         let element = totalValue[i].value
         element = parseFloat(element.split(',').join('.'))
         let displayValueNumber = parseFloat(displayValue.split(',').join('.'))
         totalPrice = totalPrice + element
         document.querySelector('#display-value').innerHTML = totalPrice.toString()
-        console.log(totalPrice)
     }
 }
 
@@ -105,13 +127,11 @@ function calculateTotalValue(qntd, price) {
             resultDecimal = `${Math.round(parseFloat(result.toString().split('.')[1].split('').slice(0, 2).join('.')))}${0}`
             resultInt = result.toString().split('.')[0]
             result = `${resultInt},${resultDecimal}`
-            console.log('decimal')
             return result.toString()
         } else {
             return `${result},00`
         }
     } else {
-        console.log('normal')
         return `${qntd * price},00`
     }
 }
@@ -144,35 +164,56 @@ function editLine(obj) {
 
 }
 
-function disableAttributeReaonly() {
-    for (let i = 0; i < arrayId.length; i++) {
-        const tr = document.querySelector(`#row${arrayId[i]}`)
-        for (let x = 0; x < 4; x++) {
-            const input = tr.children[x].querySelector('input');
-            console.log(input)
-            input.removeAttribute('readonly')
-        }
-    }
-}
+
 
 // Validation Bootstrap
 (function () {
     'use strict'
 
+    function disableAttributeReadonly() {
+        for (let i = 0; i < arrayId.length; i++) {
+            const tr = document.querySelector(`#row${arrayId[i]}`)
+            for (let x = 0; x < 3; x++) {
+                const input = tr.children[x].querySelector('input');
+                input.removeAttribute('readonly')
+            }
+        }
+    }
+
+    function tableLengthValidation() {
+
+        let rows = document.querySelector('#tbody').children.length
+
+        if (rows == 0) {
+            return ('empty')
+        }
+    }
+
+
     var forms = document.querySelectorAll('.needs-validation')
 
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
-
             form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
+
+                if (tableLengthValidation() == 'empty') {
                     event.preventDefault()
-                    event.stopPropagation()
+                    shootModal('modal-tableLenghtValidation')
+                } else {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    disableAttributeReadonly()
+
+                    form.classList.add('was-validated')
                 }
-
-                disableAttributeReaonly()
-
-                form.classList.add('was-validated')
             }, false)
         })
 })()
+
+// Senior
+function _saveData(data, info) {
+    let newData = {}
+}
